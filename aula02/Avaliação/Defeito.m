3093c238af577686094286e1771d027cc669aa18
ricardombrodriguez <69654430+ricardombrodriguez@users.noreply.github.com>
2021-03-03 19:43:05 +0000

    Delete aula02/Avaliação directory

--- aula02/Avaliação/Defeito.m
+++
@@ -1,9 +0,0 @@
-
-function probDefeito = Defeito(p1,p2,pa,x,n,N)
-    brinquedos = rand(3, n * N);
-    Defeito = zeros(n,N);
-    for k = 1:n*N
-        Defeito(k) = brinquedos(1,k) < p1 | brinquedos(2,k) < p2 | brinquedos(3,k) < pa;
-    end
-    probDefeito = sum(sum(Defeito) == x) / N;
-end
